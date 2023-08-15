d81a9e4a2260481fccbae7515386e48d66709914
M4tthewDE <mattider2@gmail.com>
2023-08-15 22:41:19 +0200

    Color diff stats

--- src/ui.rs
+++ src/ui.rs
@@ -163,12 +163,29 @@ impl Widget for StatsWidget {
         let insertion_count = self.stats.insertions;
         let deletion_count = self.stats.deletions;
 
-        let content = format!(
-            "{} file(s) changed, {} insertions(+), {} deletions(-)\n",
-            file_changed_count, insertion_count, deletion_count
-        );
+        let files_richtext = match file_changed_count {
+            1 => {
+                RichText::new(format!("{} file changed,", file_changed_count)).color(Color32::WHITE)
+            }
+            _ => RichText::new(format!("{} files changed,", file_changed_count))
+                .color(Color32::WHITE),
+        };
 
-        let richtext = RichText::new(content).color(Color32::WHITE);
-        ui.label(richtext)
+        let insertions_richtext = match insertion_count {
+            1 => RichText::new(format!("{} insertion(+),", insertion_count)).color(Color32::GREEN),
+            _ => RichText::new(format!("{} insertions(+),", insertion_count)).color(Color32::GREEN),
+        };
+
+        let deletions_richtext = match deletion_count {
+            1 => RichText::new(format!("{} deletion(-)", deletion_count)).color(Color32::RED),
+            _ => RichText::new(format!("{} deletions(-)", deletion_count)).color(Color32::RED),
+        };
+
+        ui.horizontal(|ui| {
+            ui.label(files_richtext);
+            ui.label(insertions_richtext);
+            ui.label(deletions_richtext);
+        })
+        .response
     }
 }
