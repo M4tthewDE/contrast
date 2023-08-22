4059c12a0b44545199c19e899fd48bbc587ca02d
Quinn <quinn@quinndt.ca>
2023-08-22 03:15:46 -0500

    Don't trim '\\?\' from path

--- src/main.rs
+++ src/main.rs
@@ -30,14 +30,7 @@ fn main() -> Result<(), eframe::Error> {
         Some(relative_path) => {
             let path = PathBuf::from(relative_path);
             match fs::canonicalize(path) {
-                Ok(p) => Some(PathBuf::from(
-                    p.to_str()
-                        .unwrap()
-                        .to_string()
-                        // windows, am i rite?
-                        .strip_prefix(&"\\\\?\\".to_string())
-                        .unwrap(),
-                )),
+                Ok(p) => Some(PathBuf::from(p)),
                 Err(err) => {
                     eprintln!("Invalid path: {err}");
                     None
