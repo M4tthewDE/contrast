92d391c7bdf8c9d723aaad5a39854a5befea1ef5
M4tthewDE <matthias.kronberg@gmail.com>
2024-03-05 17:33:45 +0100

    Move line struct into own file

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -2,11 +2,13 @@ use anyhow::Result;
 use core::fmt;
 use git2::Repository;
 use header::Header;
+use line::Line;
 use stats::Stats;
 use std::{cell::RefCell, path::PathBuf, rc::Rc};
 
 pub mod commit;
 mod header;
+mod line;
 pub mod stats;
 
 #[derive(Debug, Clone)]
@@ -124,36 +126,6 @@ impl fmt::Display for Diff {
         }
 
         Ok(())
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct Line {
-    pub old_lineno: Option<u32>,
-    pub new_lineno: Option<u32>,
-    pub content: String,
-    pub origin: char,
-}
-
-impl Line {
-    fn new(
-        old_lineno: Option<u32>,
-        new_lineno: Option<u32>,
-        content: String,
-        origin: char,
-    ) -> Line {
-        Line {
-            old_lineno,
-            new_lineno,
-            content,
-            origin,
-        }
-    }
-}
-
-impl fmt::Display for Line {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {}", self.origin, self.content)
     }
 }
 
