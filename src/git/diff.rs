48744d917ba5ca7b9a16ae18f215a4329b396e48
M4tthewDE <matthias.kronberg@gmail.com>
2024-03-07 18:16:28 +0100

    Use a cursor for byte parsing

--- src/git/diff.rs
+++ src/git/diff.rs
@@ -1,33 +1,38 @@
 // https://blog.jcoglan.com/2017/02/12/the-myers-diff-algorithm-part-1/
 
-use std::slice::Iter;
+use std::io::{Cursor, Read};
 
 use anyhow::{anyhow, Result};
 
 fn parse_index_file(bytes: &[u8]) -> Result<()> {
-    let signature = &bytes[0..4];
-    if signature != &[68, 73, 82, 67] {
+    let mut cursor = Cursor::new(bytes);
+    let mut signature = [0u8; 4];
+    cursor.read_exact(&mut signature)?;
+    if signature != [68, 73, 82, 67] {
         return Err(anyhow!("Invalid signature: {:?}", signature));
     }
 
-    let version = &bytes[7];
-    if !vec![2, 3, 4].contains(version) {
+    let mut version = [0u8; 4];
+    cursor.read_exact(&mut version)?;
+    let version = u32::from_be_bytes(version);
+    if ![2, 3, 4].contains(&version) {
         return Err(anyhow!("Invalid version: {:?}", version));
     }
 
-    parse_index_entry(bytes[8..].iter())?;
+    let mut index_entry_num = [0u8; 4];
+    cursor.read_exact(&mut index_entry_num)?;
+    let index_entry_num = u32::from_be_bytes(index_entry_num);
+    dbg!(index_entry_num);
+
+    parse_index_entry(&mut cursor)?;
     todo!();
 }
 
-fn parse_index_entry(bytes: Iter<u8>) -> Result<()> {
-    let seconds = bytes
-        .take(4)
-        .copied()
-        .collect::<Vec<u8>>()
-        .try_into()
-        .map(u32::from_be_bytes)
-        .map_err(|_| anyhow!("Invalid seconds"))?;
-    dbg!(seconds);
+fn parse_index_entry(cursor: &mut Cursor<&[u8]>) -> Result<()> {
+    let mut metadata_changed_secs = [0u8; 4];
+    cursor.read_exact(&mut metadata_changed_secs)?;
+    let metadata_changed_secs = u32::from_be_bytes(metadata_changed_secs);
+    dbg!(metadata_changed_secs);
     Ok(())
 }
 
