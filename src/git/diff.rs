b5efb0807c6034a7ea4d5cfaaaa17057d0a25ba6
M4tthewDE <matthias.kronberg@gmail.com>
2024-03-09 12:20:08 +0100

    WIP: index parsing

--- src/git/diff.rs
+++ src/git/diff.rs
@@ -5,6 +5,7 @@ use std::io::{Cursor, Read};
 use anyhow::{anyhow, Result};
 use chrono::{TimeZone, Utc};
 
+// https://git-scm.com/docs/index-format
 fn parse_index_file(bytes: &[u8]) -> Result<()> {
     let mut cursor = Cursor::new(bytes);
     let mut signature = [0u8; 4];
@@ -20,6 +21,8 @@ fn parse_index_file(bytes: &[u8]) -> Result<()> {
         return Err(anyhow!("Invalid version: {:?}", version));
     }
 
+    dbg!(version);
+
     let mut index_entry_num = [0u8; 4];
     cursor.read_exact(&mut index_entry_num)?;
     let index_entry_num = u32::from_be_bytes(index_entry_num);
@@ -27,6 +30,26 @@ fn parse_index_file(bytes: &[u8]) -> Result<()> {
 
     parse_index_entry(&mut cursor)?;
     todo!();
+}
+
+#[derive(Debug)]
+enum ModeType {
+    RegularFile,
+    SymbolicLink,
+    GitLink,
+}
+
+impl TryFrom<u32> for ModeType {
+    type Error = anyhow::Error;
+
+    fn try_from(value: u32) -> std::result::Result<Self, Self::Error> {
+        match value {
+            0b1000 => Ok(Self::RegularFile),
+            0b1010 => Ok(Self::SymbolicLink),
+            0b1110 => Ok(Self::GitLink),
+            _ => Err(anyhow!("Invalid mode type {value}")),
+        }
+    }
 }
 
 fn parse_index_entry(cursor: &mut Cursor<&[u8]>) -> Result<()> {
@@ -59,6 +82,34 @@ fn parse_index_entry(cursor: &mut Cursor<&[u8]>) -> Result<()> {
     cursor.read_exact(&mut ino)?;
     let ino = u32::from_be_bytes(ino);
     dbg!(ino);
+
+    let mut mode = [0u8; 4];
+    cursor.read_exact(&mut mode)?;
+    let mode = u32::from_be_bytes(mode) & 65535;
+    let mode_type = ModeType::try_from(mode >> 12)?;
+    dbg!(mode_type);
+    let permission = mode & 511;
+    // FIXME: this permission value is not valid
+    dbg!(permission);
+
+    let mut uid = [0u8; 4];
+    cursor.read_exact(&mut uid)?;
+    let uid = u32::from_be_bytes(uid);
+    dbg!(uid);
+
+    let mut gid = [0u8; 4];
+    cursor.read_exact(&mut gid)?;
+    let gid = u32::from_be_bytes(gid);
+    dbg!(gid);
+
+    let mut file_size = [0u8; 4];
+    cursor.read_exact(&mut file_size)?;
+    let file_size = u32::from_be_bytes(file_size);
+    dbg!(file_size);
+
+    let mut hash = [0u8; 5];
+    cursor.read_exact(&mut hash)?;
+    dbg!(hash);
 
     Ok(())
 }
