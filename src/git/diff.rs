2a500ac4745913122af087ee16f55e6c5236f6ca
M4tthewDE <matthias.kronberg@gmail.com>
2024-03-09 20:59:11 +0100

    Fix printing and test

--- src/git/diff.rs
+++ src/git/diff.rs
@@ -5,7 +5,7 @@ use anyhow::{anyhow, Context, Result};
 // Heavily inspired by:
 // https://blog.jcoglan.com/2017/02/12/the-myers-diff-algorithm-part-1/
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 struct DiffLine {
     number: usize,
     text: String,
@@ -13,7 +13,7 @@ struct DiffLine {
 
 impl Display for DiffLine {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "{} {}", self.number, self.text)
+        write!(f, "{} {}", self.number, self.text)
     }
 }
 
@@ -28,7 +28,7 @@ fn get_lines(content: &str) -> Vec<DiffLine> {
     let mut lines = Vec::new();
     for (i, line) in content.lines().enumerate() {
         lines.push(DiffLine {
-            number: i,
+            number: i + 1,
             text: line.to_string(),
         })
     }
@@ -36,12 +36,32 @@ fn get_lines(content: &str) -> Vec<DiffLine> {
     lines
 }
 
+#[derive(Debug, PartialEq)]
 enum EditType {
     Ins,
     Del,
     Eql,
 }
 
+impl EditType {
+    fn get_color(&self) -> String {
+        match self {
+            EditType::Ins => "\x1b[32m".to_string(),
+            EditType::Del => "\x1b[31m".to_string(),
+            EditType::Eql => "\x1b[39m".to_string(),
+        }
+    }
+
+    fn get_tag(&self) -> String {
+        match self {
+            EditType::Ins => "+".to_string(),
+            EditType::Del => "-".to_string(),
+            EditType::Eql => " ".to_string(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
 struct DiffEdit {
     typ: EditType,
     a_line: Option<DiffLine>,
@@ -50,11 +70,25 @@ struct DiffEdit {
 
 impl Display for DiffEdit {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.typ {
-            EditType::Ins => write!(f, "I {}", self.b_line.clone().unwrap()),
-            EditType::Del => write!(f, "D {}", self.a_line.clone().unwrap()),
-            EditType::Eql => write!(f, "  {}", self.a_line.clone().unwrap()),
-        }
+        let color = self.typ.get_color();
+        let reset = "\x1b[39m";
+        let tag = self.typ.get_tag();
+        let old_line = self
+            .a_line
+            .clone()
+            .map(|a| a.number.to_string())
+            .unwrap_or(" ".to_string());
+        let new_line = self
+            .b_line
+            .clone()
+            .map(|b| b.number.to_string())
+            .unwrap_or(" ".to_string());
+        let text = self
+            .a_line
+            .clone()
+            .unwrap_or_else(|| self.b_line.clone().unwrap())
+            .text;
+        write!(f, "{color}{tag} {old_line} {new_line}   {text}{reset}",)
     }
 }
 
@@ -209,18 +243,103 @@ fn set<T>(vec: &mut Vec<T>, index: isize, value: T) {
 
 #[cfg(test)]
 mod tests {
-    use super::calculate_diff;
+
+    use super::{calculate_diff, DiffEdit, DiffLine, EditType};
 
     #[test]
     fn test_calculate_diff() {
         let old = include_str!("../../tests/data/a");
         let new = include_str!("../../tests/data/b");
 
+        let expected = vec![
+            DiffEdit::new(
+                EditType::Del,
+                Some(DiffLine {
+                    number: 1,
+                    text: "A".to_string(),
+                }),
+                None,
+            ),
+            DiffEdit::new(
+                EditType::Del,
+                Some(DiffLine {
+                    number: 2,
+                    text: "B".to_string(),
+                }),
+                None,
+            ),
+            DiffEdit::new(
+                EditType::Eql,
+                Some(DiffLine {
+                    number: 3,
+                    text: "C".to_string(),
+                }),
+                Some(DiffLine {
+                    number: 1,
+                    text: "C".to_string(),
+                }),
+            ),
+            DiffEdit::new(
+                EditType::Ins,
+                None,
+                Some(DiffLine {
+                    number: 2,
+                    text: "B".to_string(),
+                }),
+            ),
+            DiffEdit::new(
+                EditType::Eql,
+                Some(DiffLine {
+                    number: 4,
+                    text: "A".to_string(),
+                }),
+                Some(DiffLine {
+                    number: 3,
+                    text: "A".to_string(),
+                }),
+            ),
+            DiffEdit::new(
+                EditType::Eql,
+                Some(DiffLine {
+                    number: 5,
+                    text: "B".to_string(),
+                }),
+                Some(DiffLine {
+                    number: 4,
+                    text: "B".to_string(),
+                }),
+            ),
+            DiffEdit::new(
+                EditType::Del,
+                Some(DiffLine {
+                    number: 6,
+                    text: "B".to_string(),
+                }),
+                None,
+            ),
+            DiffEdit::new(
+                EditType::Eql,
+                Some(DiffLine {
+                    number: 7,
+                    text: "A".to_string(),
+                }),
+                Some(DiffLine {
+                    number: 5,
+                    text: "A".to_string(),
+                }),
+            ),
+            DiffEdit::new(
+                EditType::Ins,
+                None,
+                Some(DiffLine {
+                    number: 6,
+                    text: "C".to_string(),
+                }),
+            ),
+        ];
+
         let diff = calculate_diff(old, new).unwrap();
-        dbg!(&diff.len());
-        for d in diff {
-            dbg!(format!("{}", d));
-        }
-        todo!("implement");
+        assert_eq!(diff.len(), expected.len());
+        assert_eq!(diff, expected);
     }
 }
