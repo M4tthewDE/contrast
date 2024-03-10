f59f6c238c9d480c606752725e2af4c298465d6d
M4tthewDE <matthias.kronberg@gmail.com>
2024-03-10 22:55:51 +0100

    Basic tree file parsing

--- src/git/head.rs
+++ src/git/head.rs
@@ -2,7 +2,7 @@ use anyhow::{anyhow, Result};
 use flate2::read::ZlibDecoder;
 use std::{
     fs,
-    io::{Cursor, Read},
+    io::{BufRead, Cursor, Read},
     path::PathBuf,
 };
 
@@ -22,9 +22,8 @@ fn get_latest_commit(repo: &PathBuf) -> Result<()> {
         .strip_suffix("\n")
         .ok_or(anyhow!("error parsing refs/heads/{}", head))?;
 
-    let commit = get_commit(repo, hash)?;
+    let _commit = get_commit(repo, hash)?;
     todo!();
-    Ok(())
 }
 
 fn get_commit(repo: &PathBuf, hash: &str) -> Result<()> {
@@ -53,12 +52,40 @@ fn get_commit(repo: &PathBuf, hash: &str) -> Result<()> {
         .join(tree_hash[2..].to_owned());
 
     let bytes = fs::read(tree_path)?;
+    let tree = parse_tree(&bytes);
+
+    Ok(())
+}
+
+const NUL: u8 = 0;
+const SPACE: u8 = 32;
+
+fn parse_tree(bytes: &[u8]) -> Result<()> {
     let mut decoder = ZlibDecoder::new(Cursor::new(bytes));
     let mut bytes = Vec::new();
     decoder.read_to_end(&mut bytes)?;
-    println!("{}", bytes.len());
 
-    Ok(())
+    let mut cursor = Cursor::new(bytes);
+    let mut trash = Vec::new();
+    cursor.read_until(NUL, &mut trash)?;
+
+    let mut mode = Vec::new();
+    cursor.read_until(SPACE, &mut mode)?;
+    mode.remove(mode.len() - 1);
+    let mode = String::from_utf8(mode)?;
+    dbg!(mode);
+
+    let mut name = Vec::new();
+    cursor.read_until(NUL, &mut name)?;
+    name.remove(name.len() - 1);
+    let name = String::from_utf8(name)?;
+    dbg!(name);
+
+    let mut hash = [0u8; 20];
+    cursor.read_exact(&mut hash)?;
+    let hash: String = hash.iter().map(|b| format!("{:02x}", b)).collect();
+    dbg!(hash);
+    todo!();
 }
 
 #[cfg(test)]
@@ -67,6 +94,8 @@ mod tests {
 
     use crate::git::head::{get_head, get_latest_commit};
 
+    use super::parse_tree;
+
     #[test]
     fn test_get_head() {
         let head = get_head(&PathBuf::from(".git")).unwrap();
@@ -74,7 +103,29 @@ mod tests {
     }
 
     #[test]
+    #[ignore]
     fn test_get_latest_commit() {
-        let commit = get_latest_commit(&PathBuf::from(".git")).unwrap();
+        let _commit = get_latest_commit(&PathBuf::from(".git")).unwrap();
+    }
+
+    /*
+    040000 tree 28dd2a88014c934e00646eb9744fdad4126e5ccd    .github
+    100644 blob ea8c4bf7f35f6f77f75d92ad8ce8349f6e81ddba    .gitignore
+    100644 blob 306778b4c5833eb760e38e007fcdb3e1f04bb175    Cargo.lock
+    100644 blob 92685634cfc62c8b7fb4eb65e54ae953ed6ce620    Cargo.toml
+    100644 blob f288702d2fa16d3cdf0035b15a9fcbc552cd88e7    LICENSE
+    100644 blob 7d7005a975cf30c19dd5f744bf8f1cf2731f0287    README.md
+    040000 tree 1f19b49b2b4df317e7911b3a4ccff32982cbbd08    src
+    040000 tree 798a3fa9e3a8fecb3c38ffa18beb07cb90295855    tests
+        */
+
+    /*
+    40000 .github(*LNdntOn\100644 .gitignoreK_ow]4nݺ100644 Cargo.lock0gxŃ>`ͳKu100644 Cargo.tomlhV4,eJSl 100644 LICENSEp-/m<5ZR͈100644 README.md}pu0Ds40000 src+ML)40000 testsy?<8ː)XU
+    */
+
+    #[test]
+    fn test_parse_tree() {
+        let bytes = include_bytes!("../../tests/data/tree");
+        let tree = parse_tree(bytes).unwrap();
     }
 }
